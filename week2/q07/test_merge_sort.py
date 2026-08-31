b1fa25c39b6d0e07024b920defbcaadc2f802682
wangshuai0606 <3492610662@qq.com>
2026-08-31 16:12:41 +0800

    Week2 Q07: 调试归并排序 - VS Code Python语言服务器、ruff、pytest

---
+++ week2/q07/test_merge_sort.py
@@ -0,0 +1,11 @@
+﻿from merge_sort import merge_sort
+
+def test_merge_sort():
+    assert merge_sort([3, 1, 4, 1, 5, 9, 2, 6]) == [1, 1, 2, 3, 4, 5, 6, 9]
+    assert merge_sort([]) == []
+    assert merge_sort([1]) == [1]
+    assert merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
+
+if __name__ == '__main__':
+    test_merge_sort()
+    print('All tests passed!')
